400bcf38295018eaa74b851642fd79f88091e3fe
Kiomars93 <kiomars.mirsaeedi@hotmail.com>
2024-02-04 22:26:51 +0100

    WIP

--- src/pages/Hem.tsx
+++ src/pages/Hem.tsx
@@ -1,39 +1,48 @@
-import React from "react";
-import nathalieProfil from "/nat.png";
 import "./Hem.css";
+import profilbild from "../../public/profilbild.png";
+import { ScrollRestoration } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 export default function Hem() {
   return (
     <section className="hem-container">
       <div className="hem-leftside">
-        <h1></h1>
-        <h3>Sidans beskrivning:</h3>
-        <p className="p-Content">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ad
-          suscipit totam. In eligendi dolorum magni modi laborum quia nesciunt
-          atque at nemo optio numquam quas, iure nihil delectus quos? Lorem
-          ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ad
-          suscipit totam. In eligendi dolorum magni modi laborum quia nesciunt
-          atque at nemo optio numquam quas, iure nihil delectus quos? Lorem
-          ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ad
-          suscipit totam. In eligendi dolorum magni modi laborum quia nesciunt
-          atque at nemo optio numquam quas, iure nihil delectus quos? Lorem
-          ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ad
-          suscipit totam. In eligendi dolorum magni modi laborum quia nesciunt
-          atque at nemo optio numquam quas, iure nihil delectus quos?
-        </p>
+        {/* <h1></h1> */}
+        <article>
+          <h3>Sidans beskrivning:</h3>
+          <p className="p-Content">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ad
+            suscipit totam. In eligendi dolorum magni modi laborum quia nesciunt
+            atque at nemo optio numquam quas, iure nihil delectus quos? Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ad
+            suscipit totam. In eligendi dolorum magni modi laborum quia nesciunt
+            atque at nemo optio numquam quas, iure nihil delectus quos? Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ad
+            suscipit totam. In eligendi dolorum magni modi laborum quia nesciunt
+            atque at nemo optio numquam quas, iure nihil delectus quos? Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ad
+            suscipit totam. In eligendi dolorum magni modi laborum quia nesciunt
+            atque at nemo optio numquam quas, iure nihil delectus quos?
+          </p>
+        </article>
       </div>
       <div className="hem-rightside">
-        <h3>Om mig:</h3>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ad
-          suscipit totam. In eligendi dolorum magni modi laborum quia nesciunt
-          atque at nemo optio numquam quas, iure nihil delectus quos?
-        </p>
-        <div className="hem-img">
-          <img src={nathalieProfil} className="logo" alt="Profilbild" />
+        <Col>
+          <Image className="img-profilbild" src={profilbild} alt="profilbild" />
+        </Col>
+        <div className="text-container">
+          <h4>Om mig:</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            adipisci provident voluptate, ad tenetur illo? Dignissimos
+            recusandae hic sequi, minima omnis, eveniet iure, ipsa rem
+            reprehenderit nostrum quia odio fugit!
+          </p>
         </div>
       </div>
+      {/* <ScrollRestoration /> */}
     </section>
   );
 }
--- src/pages/Kontakt.tsx
+++ src/pages/Kontakt.tsx
@@ -2,49 +2,83 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
 import "./Kontakt.css";
+import mountains from "../../public/mountains.jpg";
 
 export default function Kontakt() {
   return (
-    <Container>
-      <Row>
-        <Col xs={12} md={6} lg={4} className="kontakt-leftside">
-          <h3>Kontakt</h3>
-          <div className="span-container">
-            <span>Nathalie Kornhonen</span>
-            <span>
-              Telefon:
-              <a href="tel:+467XXXXXXXX">+467XXXXXXXX</a>
-            </span>
-            <span>
-              Email:
-              <a href="mailto:someone@example.com">someone@example.com</a>
-            </span>
-            <address>
-              <p>Winter wonderland 56B</p> <p>415 98</p> <p> Göteborg</p>
-            </address>
+    <div>
+      <Container>
+        <Row className="row-main">
+          <div className="kontakt-leftside">
+            <Col xs={4} md={6} lg={12}>
+              <h3 className="kontakt-h3">Kontakt</h3>
+              <div className="span-container">
+                <span>Nathalie Kornhonen</span>
+                <span>
+                  Telefon:
+                  <a href="tel:+467XXXXXXXX">+467XXXXXXXX</a>
+                </span>
+                <span>
+                  Email:
+                  <a href="mailto:someone@example.com">someone@example.com</a>
+                </span>
+                <address>
+                  <p>Winter wonderland 56B</p> <p>415 98</p> <p> Göteborg</p>
+                </address>
+              </div>
+            </Col>
           </div>
-        </Col>
-        <Col xs={12} md={6} lg={4} className="kontakt-rightside">
-          <form>
-            <label>
-              Namn:
-              <input name="namn" />
-            </label>
-            <label>
-              Email:
-              <input name="email" />
-            </label>
-            <div>
-              <label>
-                Kort beskrivning om vad ärendet gäller:
-                <textarea />
-              </label>
-            </div>
-            <input type="submit" value="Submit" />
-          </form>
-        </Col>
-      </Row>
-    </Container>
+          <div className="hero-image">
+            <img src={mountains} alt="ocean/100px250" />
+          </div>
+          <div className="kontakt-rightside">
+            <Col xs={12} md={6} lg={12}>
+              <Form>
+                <Form.Group as={Row} className="mb-3" controlId="formGroupNamn">
+                  <Form.Label column sm={1}>
+                    Namn:
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Control type="namn" placeholder="namn" />
+                  </Col>
+                </Form.Group>
+                <Form.Group
+                  as={Row}
+                  className="mb-3"
+                  controlId="formGroupEmail"
+                >
+                  <Form.Label column sm={1}>
+                    Email address:
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Control type="email" placeholder="email" />
+                  </Col>
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="beskrivningForm.ControlTextarea"
+                >
+                  <Form.Label>
+                    Kort beskrivning om vad ärendet gäller:
+                  </Form.Label>
+                  <Form.Control
+                    className="textarea-main"
+                    as="textarea"
+                    rows={3}
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Skicka
+                </Button>
+              </Form>
+            </Col>
+          </div>
+        </Row>
+      </Container>
+    </div>
   );
 }
