6a4db2457c46e07378668dc8a898eed9537f6709
Kiomars93 <kiomars.mirsaeedi@hotmail.com>
2024-02-17 21:02:27 +0100

    Adding more to footer with copywrites and creating a Policy tab at the bottom.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PathConstants from "../routes/PathConstants";
+import "./Header.css";
 
 export default function Footer() {
   return (
     <footer>
       <nav>
-        <ul className="">
+        <ul className="navbar-container">
+          <p>&copy; 2024 Nathalie Psykologi AB. All rights reserved.</p>
           <li>
-            <Link className="navbar-iteem" to={PathConstants.Policy}>
+            <Link className="navbar-item" to={PathConstants.Policy}>
               Policy
             </Link>
           </li>
