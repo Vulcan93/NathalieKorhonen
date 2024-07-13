067982197756df9c43c42d479ff808ed168742c8
Kiomars93 <kiomars.mirsaeedi@hotmail.com>
2024-07-13 22:55:05 +0200

    ...

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import {
-  Suspense,
+  // Suspense,
   createContext,
   useContext,
   useEffect,
